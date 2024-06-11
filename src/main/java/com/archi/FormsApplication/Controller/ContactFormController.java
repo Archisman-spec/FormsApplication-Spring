9335f51b3e0bb07070e6bbd0de0f42a5dc9c3cfb
Archisman-spec <archiarchisman@gmail.com>
2024-06-12 00:50:26 +0530

    intial commit

--- src/main/java/com/archi/FormsApplication/Controller/ContactFormController.java
+++ src/main/java/com/archi/FormsApplication/Controller/ContactFormController.java
@@ -1,8 +1,11 @@
 package com.archi.FormsApplication.Controller;
 
+import com.archi.FormsApplication.Domain.ContactForm;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,9 +15,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class ContactFormController {
 
     @RequestMapping(path = "/contact", method = RequestMethod.POST)
-    public String contactFormSubmission(@RequestBody MultiValueMap values) {
-        log.info(values.toString());
+    public String contactFormSubmission(@Valid  @ModelAttribute final ContactForm contactForm) {
 
+        log.info(contactForm.toString());
         return "redirect:success.html";
     }
 }
--- src/main/java/com/archi/FormsApplication/Domain/ContactForm.java
+++ src/main/java/com/archi/FormsApplication/Domain/ContactForm.java
@@ -1,4 +1,23 @@
 package com.archi.FormsApplication.Domain;
 
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ContactForm {
+
+    @NotEmpty
+    private String first;
+    @NotEmpty
+    private String last;
+
+
+    private String comment;
+
 }
